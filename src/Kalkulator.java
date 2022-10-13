76e66fc2a44f4f0ba643673dc1b28945d008fc31
fajar abdillah ahmad <fajardillah25@gmail.com>
2022-10-13 21:00:03 +0700

    menambah while dan beberapa syntax untuk ulang kembali

--- src/Kalkulator.java
+++ src/Kalkulator.java
@@ -1,35 +1,72 @@
 import java.util.*;
 
 public class Kalkulator {
-    void menu(){
-        System.out.println("==== Selamat Datang Di Program Kalkulator Sederhana ====");
-        System.out.println("== created by fajar abdillah ahmad ==");
-        System.out.println("1. (+)untuk penjumlahan ");
-        System.out.println("2. (-)untuk pengurangan ");
-        System.out.println("3. (/)untuk pembagian ");
-        System.out.println("4. (x)untuk perkalian ");
-        System.out.println("5. (luas)untuk mencari luas bangun datar");
+   public static void menu(){
+        while (true){
+            System.out.println("==== Selamat Datang Di Program Kalkulator Sederhana ====");
+            System.out.println("== created by fajar abdillah ahmad ==");
+            System.out.println("1. (+)untuk penjumlahan ");
+            System.out.println("2. (-)untuk pengurangan ");
+            System.out.println("3. (/)untuk pembagian ");
+            System.out.println("4. (x)untuk perkalian ");
+            System.out.println("5. (luas)untuk mencari luas bangun datar");
 
-        Scanner input = new Scanner(System.in);
-        System.out.print("Silahkan masukan operator yang di inginkan :");
-        String inputUser = input.nextLine();
+            Scanner input = new Scanner(System.in);
+            System.out.print("Silahkan masukan operator yang di inginkan :");
+            String inputUser = input.nextLine();
 
 
-        if (inputUser.equals("+")) {
-            System.out.println("** operasi penjumlahan **");
-            penjumlahan();
-        } else if (inputUser.equals("-")) {
-            System.out.println("** operasi pengurangan **");
-            pengurangan();
-        } else if (inputUser.equals("/")) {
-            System.out.println("** operasi pembagian **");
-            pembagian();
-        } else if (inputUser.equals("x")) {
-            System.out.println("** operasi perkalian **");
-            perkaian();
-        } else {
-            System.out.println(")> perintah tidak di kenal <(");
+
+            if (inputUser.equals("+")) {
+                System.out.println("** operasi penjumlahan **");
+                penjumlahan();
+                System.out.println("pilih y/n untuk mengulang : ");
+                Scanner rety = new Scanner(System.in);
+                String ulang = rety.nextLine();
+                if (ulang.equals("y")){
+                    menu();
+                }else {
+                    break;
+                }
+            } else if (inputUser.equals("-")) {
+                System.out.println("** operasi pengurangan **");
+                pengurangan();
+                System.out.println("pilih y/n untuk mengulang : ");
+                Scanner rety = new Scanner(System.in);
+                String ulang = rety.nextLine();
+                if (ulang.equals("y")){
+                    menu();
+                }else {
+                    break;
+                }
+            } else if (inputUser.equals("/")) {
+                System.out.println("** operasi pembagian **");
+                pembagian();System.out.println("pilih y/n untuk mengulang : ");
+                Scanner rety = new Scanner(System.in);
+                String ulang = rety.nextLine();
+                if (ulang.equals("y")){
+                    menu();
+                }else {
+                    break;
+                }
+            } else if (inputUser.equals("x")) {
+                System.out.println("** operasi perkalian **");
+                perkaian();
+                System.out.println("pilih y/n untuk mengulang : ");
+                Scanner rety = new Scanner(System.in);
+                String ulang = rety.nextLine();
+                if (ulang.equals("y")){
+                    menu();
+                }else {
+                    break;
+                }
+            } else {
+                System.out.println(")> perintah tidak di kenal <(");
+            }
+
         }
+
+
     }
 
     public static int penjumlahan(){
@@ -56,13 +93,13 @@ public class Kalkulator {
 
     }
 
-    public static int pembagian(){
+    public static float pembagian(){
         Scanner input = new Scanner(System.in);
         System.out.print("masukan angka pertama : ");
-        int a = input.nextInt();
+        float a = input.nextInt();
         System.out.print("masukan angka kedua : ");
-        int b = input.nextInt();
-        var hasil = a/b;
+        float b = input.nextInt();
+        float hasil = a/b;
         System.out.println("hasil operasi = "+hasil);
         return hasil;
     }
@@ -96,6 +133,7 @@ public class Kalkulator {
             if (inputUser.equals("+")) {
                 System.out.println("** operasi penjumlahan **");
                 penjumlahan();
+
             } else if (inputUser.equals("-")) {
                 System.out.println("** operasi pengurangan **");
                 pengurangan();
@@ -114,7 +152,7 @@ public class Kalkulator {
 
 
     public static void main(String[] args) {
-        menampilkanMenu();
+        menu();
 
 
 
